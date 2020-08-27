733f11047492af0ee7c503bbfaa8b6de8dd05f92
Dorian Tsai <dorian.tsai@gmail.com>
2020-08-27 12:29:51 +1000

    first commit from Dorian. Coded loadspectrum in color.py, along with unit tests in test_color.py, and initial figures in chap10.ipynb

--- machinevisiontoolbox/color.py
+++ machinevisiontoolbox/color.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+import io as io
 import numpy as np
+# import scipy as sp
 import spatialmath.base.argcheck as argcheck
+# import matplotlib.pyplot as plt  # TODO: remove later, as only used for debugging
+
+from scipy import interpolate
+from collections import namedtuple
 
 def blackbody(lam, T):
     """
@@ -29,9 +36,9 @@ def blackbody(lam, T):
     """
 
     # physical constants
-    c = 2.99792458e8  # m/s         (speed of light)
-    h = 6.626068e-34  # m2 kg / s   (Planck's constant)
-    k = 1.3806503e-23 # J K-1      (Boltzmann's constant)
+    c = 2.99792458e8   # m/s         (speed of light)
+    h = 6.626068e-34   # m2 kg / s   (Planck's constant)
+    k = 1.3806503e-23  # J K-1      (Boltzmann's constant)
 
     lam = argcheck.getvector(lam)
 
@@ -41,7 +48,94 @@ def blackbody(lam, T):
     else:
         return e
 
-# other functions from chapter 10 go here
+
+def loadspectrum(lam, filename, **kwargs):
+    """
+    Load spectrum data
+
+    :param lam: wavelength 𝜆 [m]
+    :type lam: float or array_like
+    :param filename: filename
+    :type filename: string
+    :param variable/options? #TODO - kwargs**
+    :return: interpolated irradiance and corresponding wavelength
+    :rtype: collections.namedtuple
+
+    S = LOADSPECTRUM(LAMBDA, FILENAME) is spectral data (NxD) from file
+    FILENAME interpolated to wavelengths [meters] specified in LAMBDA (Nx1).
+    The spectral data can be scalar (D=1) or vector (D>1) valued.
+
+    [S, LAMBDA] = LOADSPECTRUM(LAMBDA, FILENAME) as above but also returns the
+    passed wavelength LAMBDA.
+
+    Example::
+        #TODO
+
+    :notes:
+
+        - The file is assumed to have its first column as wavelength in metres,
+        the remainding columns are linearly interpolated and returned as
+        columns of S.
+        - The files are kept in the private folder inside the MVTB folder.
+
+    References:
+        - Robotics, Vision & Control, Section 10.1, P. Corke, Springer 2011.
+    """
+
+    
+    # parse options
+    # defaults/assertions if filename is empty
+    # tab = load data from filename
+    # s = interpolation of data in tab
+    # return s, lambda (if nargout == 2)
+
+    # check valid input
+    lam = argcheck.getvector(lam)
+
+    # check filename is a string
+    check_filename_isstr = isinstance(filename, str)
+    if check_filename_isstr is False:
+        print('Warning: input variable "filename" is not a valid string')
+
+    if not ("." in filename):
+        filename = filename + '.dat'
+
+    try:
+        # import filename, which we expect to be a .dat file
+        # columns for wavelength and irradiance
+        # assume column delimiters are whitespace, so for .csv files,
+        # replace , with ' '
+        with open(filename) as file:
+            clean_lines = (line.replace(',', ' ') for line in file)
+            # default delimiter whitespace
+            data = np.genfromtxt(clean_lines, comments='%')
+    
+        # print(data)
+    except IOError:
+        print('An exception occurred: Spectral file {} not found'.format(filename))
+
+    # do interpolation, TODO using kwargs** to pass on options information
+    # to interp1d
+    data_wavelength = data[0:, 0]
+    data_s = data[0:, 1:]
+    f = interpolate.interp1d(data_wavelength, data_s, axis=0,
+                             bounds_error=False, **kwargs)
+
+    # TODO: check lam is contained within data_wavelength for valid
+    # take min/max of lam, compared to min/max of data_wavelength
+    # interpolation range
+    s = f(lam)
+
+    # debug: plot the data:
+    # plt.plot(data_wavelength, data_irradiance, 'o',
+    #        lam, irradiance_interp, '-')
+    # plt.grid(True)
+    # plt.xlabel('wavelength [m]')
+    # plt.ylabel('irradiance [W/m^2/m]')
+    # plt.show()
+
+    return namedtuple('spectrum', 's lam')(s, lam)
+
 
 if __name__ == '__main__':  # pragma: no cover
     import pathlib
--- machinevisiontoolbox/test_color.py
+++ machinevisiontoolbox/test_color.py
@@ -1,7 +1,11 @@
+import numpy as np
+
 import numpy.testing as nt
 import unittest
+from pathlib import Path
 
 from color import *
+
 
 class TestColor(unittest.TestCase):
 
@@ -13,10 +17,28 @@ class TestColor(unittest.TestCase):
         self.assertEqual(len(e), 2)
         self.assertAlmostEqual(e[0], 1.44662486e+12, delta=1e4)
         self.assertAlmostEqual(e[1], 2.86512308e+12, delta=1e4)
-        
 
-    # other functions from chapter 10 go here, one test method per function
+    def test_loadspectrum(self):
+        # test with filename = "data/solar.dat"
+        nm = 1e-9
+        lam = np.linspace(400, 700, 30) * nm
+        brick_spectrum = loadspectrum(lam, Path('data') / 'redbrick.dat')
+        self.assertEqual(brick_spectrum.ir.shape, (30, 1))
+        nt.assert_array_almost_equal(lam, brick_spectrum.lam)
 
+        cone_spectrum = loadspectrum(lam, 'data/cones.dat')
+        self.assertEqual(cone_spectrum.ir.shape, (30, 3))
+
+        # tests outside of interpolation range
+        lam2 = np.linspace(300, 1000, 50) * nm
+        solar_spectrum = loadspectrum(lam2, 'data/solar')
+        self.assertEqual(solar_spectrum.ir.shape, (50, 1))
+
+        lam_water = np.linspace(400, 700, 30) * nm
+        water_spectrum = loadspectrum(lam_water, 'data/water')
+
+        # TODO check interp1d options
+        b = 1
 
 # ---------------------------------------------------------------------------------------#
 if __name__ == '__main__':
