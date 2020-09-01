cc76c71ceb62155f16d8c5c5de74d1dee12ceee9
Dorian Tsai <dorian.tsai@gmail.com>
2020-09-01 10:42:42 +1000

    comments updated for Python/Sphinx documentation

--- machinevisiontoolbox/color.py
+++ machinevisiontoolbox/color.py
@@ -59,37 +59,29 @@ def loadspectrum(lam, filename, **kwargs):
     :type lam: float or array_like
     :param filename: filename
     :type filename: string
-    :param variable/options? #TODO - kwargs**
-    :return: interpolated spectrum and corresponding wavelength
+    :param kwargs**: keyword arguments for scipy.interpolate.interp1d
+    :return: interpolated spectral data and corresponding wavelength
     :rtype: collections.namedtuple
 
-    S = LOADSPECTRUM(LAMBDA, FILENAME) is spectral data (NxD) from file
-    FILENAME interpolated to wavelengths [meters] specified in LAMBDA (Nx1).
+    ``loadspectrum(𝜆, filename, **kwargs)`` is spectral data (N,D) from file
+    filename interpolated to wavelengths [meters] specified in 𝜆 (N,1).
     The spectral data can be scalar (D=1) or vector (D>1) valued.
 
-    [S, LAMBDA] = LOADSPECTRUM(LAMBDA, FILENAME) as above but also returns the
-    passed wavelength LAMBDA.
-
     Example::
+
         #TODO
 
     :notes:
 
         - The file is assumed to have its first column as wavelength in metres,
-        the remainding columns are linearly interpolated and returned as
-        columns of S.
+          the remainding columns are linearly interpolated and returned as
+          columns of S.
         - The files are kept in the private folder inside the MVTB folder.
 
     References:
+
         - Robotics, Vision & Control, Section 10.1, P. Corke, Springer 2011.
     """
-
-    
-    # parse options
-    # defaults/assertions if filename is empty
-    # tab = load data from filename
-    # s = interpolation of data in tab
-    # return s, lambda (if nargout == 2)
 
     # check valid input
     lam = argcheck.getvector(lam)
@@ -104,20 +96,17 @@ def loadspectrum(lam, filename, **kwargs):
 
     try:
         # import filename, which we expect to be a .dat file
-        # columns for wavelength and irradiance
+        # columns for wavelength and spectral data
         # assume column delimiters are whitespace, so for .csv files,
         # replace , with ' '
         with open(filename) as file:
             clean_lines = (line.replace(',', ' ') for line in file)
             # default delimiter whitespace
             data = np.genfromtxt(clean_lines, comments='%')
-    
-        # print(data)
     except IOError:
         print('An exception occurred: Spectral file {} not found'.format(filename))
 
-    # do interpolation, TODO using kwargs** to pass on options information
-    # to interp1d
+    # interpolate data
     data_wavelength = data[0:, 0]
     data_s = data[0:, 1:]
     f = interpolate.interp1d(data_wavelength, data_s, axis=0,
@@ -127,14 +116,6 @@ def loadspectrum(lam, filename, **kwargs):
     # take min/max of lam, compared to min/max of data_wavelength
     # interpolation range
     s = f(lam)
-
-    # debug: plot the data:
-    # plt.plot(data_wavelength, data_irradiance, 'o',
-    #        lam, irradiance_interp, '-')
-    # plt.grid(True)
-    # plt.xlabel('wavelength [m]')
-    # plt.ylabel('irradiance [W/m^2/m]')
-    # plt.show()
 
     return namedtuple('spectrum', 's lam')(s, lam)
 
@@ -146,50 +127,47 @@ def lambda2rg(lam, e=None):
     :param lam: wavelength 𝜆 [m]
     :type lam: float or array_like
     :param e: illlumination spectrum defined at the wavelengths 𝜆
-    :type e: numpy array (Nx1)
-    :return rgb: rg-chromaticity
-    :rtype: collections.namedtuple
+    :type e: numpy array (N,1)
+    :return rg: rg-chromaticity
+    :rtype:
 
-    RGB = LAMBDA2RG(LAMBDA) is the rg-chromaticity coordinate (1x2) for 
-    illumination at the specific wavelength LAMBDA [m]. If LAMBDA is a
-    vector (Nx1), then P (Nx2) is a vector whose elements are the chromaticity
-    coordinates at the corresponding elements of LAMBDA.
+    ``lambda2rg(𝜆)`` is the rg-chromaticity coordinate (1,2) for
+    illumination at the specific wavelength 𝜆 [m]. If 𝜆 is a
+    numpy array (N,1), then P (N,2) is a vector whose elements are the
+    chromaticity coordinates at the corresponding elements of 𝜆.
 
-    RGB = LAMBDA2RG(LAMBDA, E) is the rg-chromaticity coordinate (1x2) for an 
-    illumination spectrum E (Nx1) defined at corresponding wavelengths
-    LAMBDA (Nx1).
+    ``lambda2rg(𝜆, e)`` is the rg-chromaticity coordinate (1,2) for an
+    illumination spectrum e (N,1) defined at corresponding wavelengths
+    𝜆 (N,1).
 
     Example::
+
         #TODO
 
     :notes:
+
     - Data from http://cvrl.ioo.ucl.ac.uk
     - From Table I(5.5.3) of Wyszecki & Stiles (1982). (Table 1(5.5.3)
-    of Wyszecki & Stiles (1982) gives the Stiles & Burch functions in
-    250 cm-1 steps, while Table I(5.5.3) of Wyszecki & Stiles (1982)
-    gives them in interpolated 1 nm steps.)
+      of Wyszecki & Stiles (1982) gives the Stiles & Burch functions in
+      250 cm-1 steps, while Table I(5.5.3) of Wyszecki & Stiles (1982)
+      gives them in interpolated 1 nm steps.)
     - The Stiles & Burch 2-deg CMFs are based on measurements made on
-    10 observers. The data are referred to as pilot data, but probably
-    represent the best estimate of the 2 deg CMFs, since, unlike the CIE
-    2 deg functions (which were reconstructed from chromaticity data),
-    they were measured directly.
+      10 observers. The data are referred to as pilot data, but probably
+      represent the best estimate of the 2 deg CMFs, since, unlike the CIE
+      2 deg functions (which were reconstructed from chromaticity data),
+      they were measured directly.
     - These CMFs differ slightly from those of Stiles & Burch (1955). As
-    noted in footnote a on p. 335 of Table 1(5.5.3) of Wyszecki &
-    Stiles (1982), the CMFs have been "corrected in accordance with
-    instructions given by Stiles & Burch (1959)" and renormalized to
-    primaries at 15500 (645.16), 19000 (526.32), and 22500 (444.44) cm-1
+      noted in footnote a on p. 335 of Table 1(5.5.3) of Wyszecki &
+      Stiles (1982), the CMFs have been "corrected in accordance with
+      instructions given by Stiles & Burch (1959)" and renormalized to
+      primaries at 15500 (645.16), 19000 (526.32), and 22500 (444.44) cm-1
 
     References:
+
         - Robotics, Vision & Control, Section 10.2, P. Corke, Springer 2011.
     """
 
-    # check for arguments
-    # call cmfrgb(lambda)
-    # or RGB = cmfrgb(lambda, e)
-    # connected components tristim2cc(RGB)
-    # output rg as named tuple
-
-    # check/parse args
+    # check input
     lam = argcheck.getvector(lam)
 
     if e is None:
@@ -212,56 +190,34 @@ def cmfrgb(lam, e=None):
     :param lam: wavelength 𝜆 [m]
     :type lam: float or array_like
     :param e: illlumination spectrum defined at the wavelengths 𝜆
-    :type e: numpy array (Nx1)
-    :return rg: rg-chromaticity
-    :rtype: collections.namedtuple
+    :type e: numpy array (N,1)
+    :return rgb: rg-chromaticity
+    :rtype: numpy array, shape = (N,3)
 
-    RGB = CMFRGB(LAMBDA) is the CIE color matching function (Nx3) for illumination
-    at wavelength LAMBDA (Nx1) [m].  If LAMBDA is a vector then each row of RGB
-    is the color matching function of the corresponding element of LAMBDA. 
+    ``rgb = cmfrgb(𝜆)`` is the CIE color matching function (N,3)
+    for illumination at wavelength 𝜆 (N,1) [m]. If 𝜆 is a vector
+    then each row of RGB is the color matching function of the
+    corresponding element of 𝜆.
 
-    RGB = CMFRGB(LAMBDA, E) is the CIE color matching (1x3) function for an 
-    illumination spectrum E (Nx1) defined at corresponding wavelengths
-    LAMBDA (Nx1).
+    ``rgb = cmfrgb(𝜆, e) is the CIE color matching (1,3) function for an
+    illumination spectrum e (N,1) defined at corresponding wavelengths
+    𝜆 (N,1).
 
     Example::
+
         #TODO
 
     References:
-        - Robotics, Vision & Control, Section 10.2, P. Corke, Springer 2011.
+
+        - Robotics, Vision & Control, Chapter 10, P. Corke, Springer 2011.
     """
 
-    # parse args:
-    # TODO: should probably return an error if too many/insufficient args,
-    # or if args are invalid
-    # lam = args[0]
-    # if len(args) == 2:
-    #    e = args[1]
-    #    e = argcheck.getvector(e)
-
-    # if len(args) > 2:
-    #    print('IOError: cmfrgb expects no more than 2 input arguments')
-
-    lam = argcheck.getvector(lam)  # lam is Nx1
+    lam = argcheck.getvector(lam)  # lam is (N,1)
 
     cmfrgb_data = Path('data') / 'cmfrgb.dat'
     rgb = loadspectrum(lam, cmfrgb_data.as_posix())
     ret = rgb.s
 
-    # if len(args) == 2:
-    # approximate rectangular integration
-    if e is not None:
-        e = argcheck.getvector(e)  # e is a vector Nx1
-        e = np.expand_dims(e, 1)
-        dlam = lam[1] - lam[0]
-        ret = np.dot(e.T, ret.T) / ret.shape[0] * dlam
-
-    return ret
-    cmfrgb_data = Path('data') / 'cmfrgb.dat'
-    rgb = loadspectrum(lam, cmfrgb_data.as_posix())
-    ret = rgb.s
-
-    # if len(args) == 2:
     # approximate rectangular integration
     if e is not None:
         e = argcheck.getvector(e)  # e is a vector Nx1
@@ -274,33 +230,29 @@ def cmfrgb(lam, e=None):
 
 def tristim2cc(tri):
     """
-    tristim2cc Tristimulus to chromaticity coordinates
+    Tristimulus to chromaticity coordinates
 
-    :param tri: xyz as an array, Nx3
+    :param tri: xyz as an array, (N,3) or (N,M,3)
     :type tri: float or array_like
-    :return cc: chromaticity coordinate (1x2) cc.r, cc.g (Nx2), or IM
-    :rtype: collections.namedtuple
+    :return cc: chromaticity coordinates
+    :rtype: numpy array, shape = (N,2) or (N,M,2)
 
-    CC = TRISTIM2CC(TRI) is the chromaticity coordinate (1x2) corresponding to the
-    tristimulus TRI (1x3).  If TRI is RGB then CC is rg, if TRI is XYZ then
-    CC is xy.  Multiple tristimulus values can be given as rows of TRI (Nx3)
-    in which case the chromaticity coordinates are the corresponding rows
-    of CC (Nx2)
+    ``tristim2cc(tri)`` is the chromaticity coordinate (1x2) corresponding
+    to the tristimulus ``tri`` (1,3). Multiple tristimulus values can be
+    given as rows of ``tri`` (N,3), in which case the chromaticity
+    coordinates are the corresponding rows (N,2).
 
-    [C1,C2] = TRISTIM2CC(TRI) as above but the chromaticity coordinates are
-    returned in separate vectors, each Nx1.
-
-    OUT = TRISTIM2CC(IM) is the chromaticity coordinates corresponding to every
-    pixel in the tristimulus image IM (HxWx3).  OUT (HxWx2) has planes
-    corresponding to r and g, or x and y.
-
-    [O1,O2] = TRISTIM2CC(IM) as above but the chromaticity is returned as
-    separate images (HxW).
+    ``tristim2cc(im)`` is the chromaticity coordinates corresponding to every
+    pixel in the tristimulus image ``im`` (H,W,3). The return is (H,W,2) that
+    has planes corresponding to r and g, or x and y (depending on whether the
+    input image was rgb or xyz).
 
     Example::
+
         #TODO
 
     References:
+
         - Robotics, Vision & Control, Chapter 10, P. Corke, Springer 2011.
     """
 
@@ -317,9 +269,9 @@ def tristim2cc(tri):
         # TODO decide on output format cc.c1, cc.c2?
     else:
         # tri is given as an image
-        s = np.sum(tri, axis=2)
+        s = np.sum(tri, axis=2)  # np.tile?
         ss = np.stack((s, s), axis=-1)
-        cc = tri[0:, 0:, 0:2] / ss
+        cc = tri[0:, 0:, 0:2] / ss  # / [s s]
         # TODO decide on output format a, b as separate channels or overall cc?
 
     return cc
@@ -327,23 +279,19 @@ def tristim2cc(tri):
 
 def lambda2xy(lam, *args):
     """
-    lambda2xy xy-chromaticity coordinates for a given wavelength
+    xy-chromaticity coordinates for a given wavelength 𝜆 [meters]
 
-    :param tri: xyz as an array, Nx3
-    :type tri: float or array_like
-    :return cc: chromaticity coordinate (1x2) cc.r, cc.g (Nx2), or IM
-    :rtype: collections.namedtuple
+    :param lam: wavelength 𝜆 [m]
+    :type lam: float or array_like
+    :return xy: xy-chromaticity coordinates
+    :rtype: numpy array, shape = (N,2)
 
-    XY = LAMBDA2XY(LAMBDA) is the xy-chromaticity coordinate (1x2) for
-    illumination at the specific wavelength LAMBDA [metres]. If LAMBDA is a
-    vector (Nx1), then P (Nx2) is a vector whose elements are the luminosity
-    at the corresponding elements of LAMBDA.
-
-    XY = LAMBDA2XY(LAMBDA, E) is the rg-chromaticity coordinate (1x2) for an
-    illumination spectrum E (Nx1) defined at corresponding wavelengths
-    LAMBDA (Nx1).
+    ``lambda2xy(𝜆)`` is the xy-chromaticity coordinate (1,2) for
+    illumination at the specific wavelength 𝜆 [metres]. If 𝜆 is a
+    vector (N,1), then the return is a vector (N,2) whose elements
 
     Example::
+
         #TODO
 
     References:
@@ -352,36 +300,36 @@ def lambda2xy(lam, *args):
 
     # argcheck
     lam = argcheck.getvector(lam)
+
     cmf = cmfxyz(lam, *args)
     xy = tristim2cc(cmf)
 
-    # TODO decide on output format
     return xy
 
 
 def cmfxyz(lam, e=None):
     """
-    cmfxyz color matching function
+    color matching function for xyz tristimulus
 
     :param lam: wavelength 𝜆 [m]
     :type lam: float or array_like
     :param e: illlumination spectrum defined at the wavelengths 𝜆
-    :type e: numpy array (Nx1)
-    :return rg: rg-chromaticity
-    :rtype: collections.namedtuple
+    :type e: numpy array (N,1)
+    :return xy: xy-chromaticity
+    :rtype: numpy array, shape = (N,2)
 
     The color matching function is the XYZ tristimulus required to match a
     particular wavelength excitation.
 
-    XYZ = CMFXYZ(LAMBDA) is the CIE XYZ color matching function (Nx3) for illumination
-    at wavelength LAMBDA (Nx1) [m].  If LAMBDA is a vector then each row of XYZ
-    is the color matching function of the corresponding element of LAMBDA.
+    ``cmfxyz(𝜆)`` is the CIE XYZ color matching function (N,3) for illumination
+    at wavelength 𝜆 (N,1) [m].  If 𝜆 is a vector then each row of XYZ
+    is the color matching function of the corresponding element of 𝜆.
 
-    XYZ = CMFXYZ(LAMBDA, E) is the CIE XYZ color matching (1x3) function for an
-    illumination spectrum E (Nx1) defined at corresponding wavelengths
-    LAMBDA (Nx1).
+    ``cmfxzy(𝜆, e)`` is the CIE XYZ color matching (1,3) function for an
+    illumination spectrum e (N,1) defined at corresponding wavelengths 𝜆 (N,1).
 
     Example::
+
         #TODO
 
     :notes:
@@ -392,13 +340,12 @@ def cmfxyz(lam, e=None):
     """
 
     lam = argcheck.getvector(lam)
-    if True:
-        # import ciedat
-        ciedat = np.array([0, 0, 0, 0])
-    else:
-        ciedat = np.array([0, 0, 0, 0])
+    cmfxyz_data = Path('data') / 'cmfxyz.dat'
+    xyz = loadspectrum(lam, cmfxyz_data.as_posix())
+    ret = xyz.s
 
-    xyz = interpolate.pchip_interpolate(ciedat[0:, 0]*1e-9, ciedat[0:, 1:4], lam, axis=1, der=0)
+    xyz = interpolate.pchip_interpolate(ret[0:, 0]*1e-9, ret[0:, 0:], lam,
+                                        axis=1, der=0)
 
     if e is None:
         # approximate rectangular integration
@@ -408,7 +355,7 @@ def cmfxyz(lam, e=None):
     return xyz
 
 
-def showcolorspace(*args):
+def showcolorspace(somestr=default, *args):
     """
     showcolorspace display spectral locus
 
@@ -426,6 +373,8 @@ def showcolorspace(*args):
     :rtype: vector or 1D array
     :return AY: corresponding y-axis coordinates for IM
     :rtype: vector of 1D array
+
+    # TODO: for now, just return plotting
 
     Example::
         #TODO
