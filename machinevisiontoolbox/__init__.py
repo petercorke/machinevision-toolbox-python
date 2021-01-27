2c7738375df002b4a88078995b78b80b4e1dc3b6
Peter Corke <peter.i.corke@gmail.com>
2021-01-28 08:54:46 +1000

    specify names exported by base, pull these into mvtb

--- machinevisiontoolbox/__init__.py
+++ machinevisiontoolbox/__init__.py
@@ -3,3 +3,4 @@ from machinevisiontoolbox.Image import Image
 from machinevisiontoolbox.blobs import Blob
 from machinevisiontoolbox.features2d import *
 from machinevisiontoolbox.Camera import *
+from machinevisiontoolbox.base import *
--- machinevisiontoolbox/base/__init__.py
+++ machinevisiontoolbox/base/__init__.py
@@ -3,3 +3,47 @@ from machinevisiontoolbox.base.color import *
 from machinevisiontoolbox.base.imageio import *
 from machinevisiontoolbox.base.shapes import *
 from machinevisiontoolbox.base.graphics import *
+
+__all__ = [
+    # color
+    'blackbody',
+    'loadspectrum',
+    'lambda2rg',
+    'cmfrgb',
+    'tristim2cc',
+    'lambda2xy',
+    'cmfxyz',
+    'luminos',
+    'rluminos',
+    'ccxyz',
+    'color_bgr',
+    'colorname',
+    'showcolorspace',
+    'cie_primaries',
+    'colorconvert',
+    'gamma_encode',
+    'gamma_decode',
+
+    # graphics
+    'plot_box',
+    'plot_labelbox',
+    'plot_point',
+    'plot_text',
+    'draw_box',
+    'draw_labelbox',
+    'draw_point',
+    'draw_text',
+
+    # imageio
+    'idisp',
+    'iread',
+    'int_image',
+    'float_image',
+    'iwrite',
+
+    # shapes
+    'mkcube',
+    'mksphere',
+    'mkcylinder',
+    'mkgrid'
+]
