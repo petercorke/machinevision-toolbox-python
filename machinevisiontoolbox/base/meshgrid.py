231d835c8e4edf4787a17d71a768cc2e9129b853
Peter Corke <peter.i.corke@gmail.com>
2021-07-27 19:46:57 +1000

    add method to rotate spherical image

--- machinevisiontoolbox/base/meshgrid.py
+++ machinevisiontoolbox/base/meshgrid.py
@@ -5,3 +5,26 @@ def meshgrid(width, height):
     v = np.arange(height)
 
     return np.meshgrid(u, v)#, indexing='ij')
+
+def sphere_rotate(Phi, Theta, T):
+
+    # convert the spherical coordinates to Cartesian
+    x = np.sin(Theta) * np.cos(Phi)
+    y = np.sin(Theta) * np.sin(Phi)
+    z = np.cos(Theta)
+
+    # convert to 3xN format
+    p = np.array([x.ravel(), y.ravel(), z.ravel()])
+
+    # transform the points
+    p = T * p
+
+    # convert back to Cartesian coordinate matrices
+    x = p[0, :].reshape(x.shape)
+    y = p[1, :].reshape(x.shape)
+    z = p[2, :].reshape(x.shape)
+
+    nTheta = np.arccos(z)
+    nPhi = np.arctan2(y, x)
+
+    return nPhi, nTheta
