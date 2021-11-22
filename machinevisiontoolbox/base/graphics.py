0993fd9739d0534d2517b81dac6b2e828b0e6319
Peter Corke <peter.i.corke@gmail.com>
2021-11-22 21:30:52 +1000

    remove primitives already in SMTB

--- machinevisiontoolbox/base/graphics.py
+++ machinevisiontoolbox/base/graphics.py
@@ -4,99 +4,6 @@ from ansitable import ANSITable, Column
 from machinevisiontoolbox.base import color_bgr
 import matplotlib.pyplot as plt
 import numpy as np
-from spatialmath import base
-
-def plot_box(color, 
-        ax=None, 
-        bbox=None, 
-        bl=None, tl=None, br=None, tr=None,
-        l=None, r=None, t=None, b=None,
-        wh=None, centre=None,
-        fillcolor=None, alpha=None, thickness=None, **kwargs):
-    """
-    Plot a box using matplotlib
-
-    :param ax: the axes to draw on, defaults to ``gca()``
-    :type ax: Axis, optional
-    :param bbox: bounding box matrix, defaults to None
-    :type bbox: ndarray(2,2), optional
-    :param bl: bottom-left corner, defaults to None
-    :type bl: array_like(2), optional
-    :param tl: top-left corner, defaults to None
-    :type tl: [array_like(2), optional
-    :param br: bottom-right corner, defaults to None
-    :type br: array_like(2), optional
-    :param tr: top -ight corner, defaults to None
-    :type tr: array_like(2), optional
-    :param wh: width and height, defaults to None
-    :type wh: array_like(2), optional
-    :param centre: [description], defaults to None
-    :type centre: array_like(2), optional
-    :param color: box outline color
-    :type color: array_like(3) or str
-    :param fillcolor: box fill color
-    :type fillcolor: array_like(3) or str
-    :param alpha: transparency, defaults to 1
-    :type alpha: float, optional
-    :param thickness: line thickness, defaults to None
-    :type thickness: float, optional
-    :return: the matplotlib object
-    :rtype: Patch.Rectangle
-
-    Plots a box on the specified axes using matplotlib
-
-    The box can be specified in many ways:
-
-    - bounding box which is a 2x2 matrix [xmin, xmax; ymin, ymax]
-    - centre and width+height
-    - bottom-left and top-right corners
-    - bottom-left corner and width+height
-    - top-right corner and width+height
-    - top-left corner and width+height
-    """
-
-    if bbox is not None:
-        xy = bbox[:,0]
-        w = bbox[0,1] - bbox[0,0]
-        h = bbox[1,1] - bbox[1,0]
-    elif bl is not None and tl is None and tr is None and wh is not None and centre is None:
-        # bl + wh
-        tl = (bl[0], bl[1] - wh[1])
-        w, h = wh
-    elif bl is not None and tl is None and tr is not None and wh is None and centre is None:
-        # bl + tr
-        tl = (bl[0], tr[1])
-        w = br[0] - bl[0]
-        h = br[1] - bl[1]
-    elif tl is not None and bl is None and tr is None and wh is None and centre is None:
-        # tl + wh
-        w, h = wh
-    elif bl is None and tl is None and tr is None and wh is not None and centre is not None:
-        # centre + wh
-        w, h = wh
-        bl = (centre[0] - w / 2, centre[1] - h / 2)
-    elif bl is None and tl is None and tr is not None and wh is not None and centre is None:
-        # tr + wh
-        w, h = wh
-        tl = (tr[0] - wh[0], tr[1])
-    elif bl is None and tl is not None and tr is None and wh is not None and centre is None:
-        # tl + wh
-        w, h = wh
-    elif l is not None and r is not None and t is not None and b is not None:
-        tl = (l, t)
-        w = r - l
-        h = b - t 
-
-    if ax is None:
-        ax = plt.gca()
-
-    fill = fillcolor is not None
-    rect = plt.Rectangle(tl, w, h, edgecolor=color, facecolor=fillcolor, fill=fill,
-    alpha=alpha, linewidth=thickness, clip_on=True)
-    ax.add_patch(rect)
-    plt.draw()
-
-    return rect
 
 
 def draw_box(image, 
@@ -260,32 +167,6 @@ def draw_labelbox(image, text, textcolor='black',
     draw_text(image, (bl[0] + h2, bl[1] - h2), text, color=textcolor,
         font=font, fontsize=fontsize, fontthickness=fontthickness)
 
-def plot_text(pos, text=None, ax=None, color=None, **kwargs):
-    """
-    Plot text using matplotlib
-
-    :param pos: position of text
-    :type pos: array_like(2)
-    :param text: text
-    :type text: str
-    :param ax: axes to draw in, defaults to ``gca()``
-    :type ax: Axis, optional
-    :param color: text color, defaults to None
-    :type color: str or array_like(3), optional
-    :param kwargs: additional arguments passed to ``pyplot.text()``
-    """
-    
-    defaults = {
-        'horizontalalignment': 'left',
-        'verticalalignment': 'center'
-    }
-    for k, v in defaults.items():
-        if k not in kwargs:
-            kwargs[k] = v
-    if ax is None:
-        ax = plt.gca()
-    plt.text(pos[0], pos[1], text, color=color, **kwargs)
-
 def draw_text(image, pos, text=None, color=None, font=cv.FONT_HERSHEY_SIMPLEX, fontsize=0.3, fontthickness=2):
     """
     Draw text in image using OpenCV
@@ -315,75 +196,6 @@ def draw_text(image, pos, text=None, color=None, font=cv.FONT_HERSHEY_SIMPLEX, f
         color = color_bgr(color)
 
     cv.putText(image, text, pos, font, fontsize, color, fontthickness)
-
-
-def plot_point(pos, marker='bs', text=None, ax=None, color=None, **kwargs):
-    """
-    Plot a point using matplotlib
-
-    :param pos: position of marker
-    :type pos: array_like(2), ndarray(2,n), list of 2-tuples
-    :param marker: matplotlub marker style, defaults to 'bs'
-    :type marker: str or list of str, optional
-    :param text: text label, defaults to None
-    :type text: str, optional
-    :param ax: axes to plot in, defaults to ``gca()````
-    :type ax: Axis, optional
-    :param color: text color, defaults to None
-    :type color: str or array_like(3), optional
-
-    The color of the marker can be different to the color of the text,
-    the marker color is specified by a single letter in the marker string.
-
-    A point can multiple markers which will be overlaid, for instance ``["rx",
-    "ro"]`` will give a ⨂ symbol.
-
-    The optional text label is placed to the right of the marker, and vertically
-    aligned. 
-    
-    Multiple points can be marked if ``pos`` is a 2xn array or a list of
-    coordinate pairs.  If a label is provided every point will have the same
-    label. However, the text is processed with ``format`` and is provided with a
-    single argument, the point index (starting at zero).
-
-
-    """
-    
-    if isinstance(pos, np.ndarray):
-        if pos.ndim == 1:
-            x = pos[0]
-            y = pos[1]
-        elif pos.ndim == 2 and pos.shape[0] == 2:
-            x = pos[0,:]
-            y = pos[1,:]
-    elif isinstance(pos, (tuple, list)):
-        # [x, y]
-        # [(x,y), (x,y), ...]
-        # [xlist, ylist]
-        # [xarray, yarray]
-        if base.islistof(pos, (tuple, list)):
-            x = [z[0] for z in pos]
-            y = [z[1] for z in pos]
-        elif base.islistof(pos, np.ndarray):
-            x = pos[0]
-            y = pos[1]
-        else:
-            x = pos[0]
-            y = pos[1]
-
-    if ax is None:
-        ax = plt.gca()
-    if isinstance(marker, (list, tuple)):
-        for m in marker:
-            plt.plot(x, y, m, **kwargs)
-    else:
-        plt.plot(x, y, marker, **kwargs)
-    if text:
-        try:
-            for i, xy in enumerate(zip(x, y)):
-                plt.text(xy[0], xy[1], ' ' + text.format(i), horizontalalignment='left', verticalalignment='center', color=color, **kwargs)
-        except:
-            plt.text(x, y, ' ' + text, horizontalalignment='left', verticalalignment='center', color=color, **kwargs)
 
 def draw_point(image, pos, marker='+', text=None, color=None, font=cv.FONT_HERSHEY_SIMPLEX, fontsize=0.3, fontthickness=2):
     """
@@ -439,27 +251,27 @@ def draw_point(image, pos, marker='+', text=None, color=None, font=cv.FONT_HERSH
             s += ' ' + text.format(i)
         cv.putText(image, s, xy, font, fontsize, color, fontthickness)
 
-def plot_histogram(c, n, clip=False, ax=None, block=False, xlabel=None, ylabel=None, grid=False, **kwargs):
-    if ax is None:
-        plt.figure()
-        ax = plt.gca()
+# def plot_histogram(c, n, clip=False, ax=None, block=False, xlabel=None, ylabel=None, grid=False, **kwargs):
+#     if ax is None:
+#         plt.figure()
+#         ax = plt.gca()
 
-    # n = hist.h  # number of pixels per class
-    # c = hist.x  # class value
+#     # n = hist.h  # number of pixels per class
+#     # c = hist.x  # class value
 
-    if clip:
-        nz, _ = np.where(n > 0)
-        start = nz[0]
-        end = nz[-1] + 1
-        n = n[start:end]
-        c = c[start:end]
+#     if clip:
+#         nz, _ = np.where(n > 0)
+#         start = nz[0]
+#         end = nz[-1] + 1
+#         n = n[start:end]
+#         c = c[start:end]
 
-    ax.bar(c, n, **kwargs)
-    if xlabel is not None:
-        ax.set_xlabel(xlabel)
-    if ylabel is not None:
-        ax.set_ylabel(ylabel)
-    ax.grid(grid)
+#     ax.bar(c, n, **kwargs)
+#     if xlabel is not None:
+#         ax.set_xlabel(xlabel)
+#     if ylabel is not None:
+#         ax.set_ylabel(ylabel)
+#     ax.grid(grid)
 
     # plt.show(block=block)
 
