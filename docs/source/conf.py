99413dfa3cbf56eec21a9a6457d36b0996435fdf
Peter Corke <peter.i.corke@gmail.com>
2023-01-23 20:53:11 +1000

    bump version for release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020-, Peter Corke'
 author = 'Peter Corke and Dorian Tsai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
