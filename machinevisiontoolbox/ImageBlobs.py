adf11cb37da73675730610258de87b212fd01d05
Peter Corke <peter.i.corke@gmail.com>
2024-05-26 16:36:39 +1000

    PR #15 which I incorrectly modified and merged

--- machinevisiontoolbox/ImageBlobs.py
+++ machinevisiontoolbox/ImageBlobs.py
@@ -237,8 +237,10 @@ class Blobs(UserList):  # lgtm[py/missing-equals]
 
             blob.contourpoint = blob.perimeter[:, 0]
 
-            ## append the new Blob instance
-            self.data.append(blob)
+            ## For a single set pixel OpenCV returns all moments as zero, skip such blobs
+            ## TODO handle this situation by setting m00=1, m10=x, m01=y etc.
+            if blob.moments["m00"] != 0:
+                self.data.append(blob)
 
         ## second pass: equivalent ellipse
 
