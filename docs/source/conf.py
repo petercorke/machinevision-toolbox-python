5292fb07f88aca826d707f45d66595065c49c271
Peter Corke <peter.i.corke@gmail.com>
2024-12-24 15:13:01 +1100

    update intersphinx links for SMTB and matplotlib

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,19 +12,20 @@
 #
 import os
 import sys
+
 # sys.path.insert(0, os.path.abspath('../../machinevisiontoolbox'))
-sys.path.append(os.path.abspath('exts'))
-print(os.path.abspath('exts'))
+sys.path.append(os.path.abspath("exts"))
+print(os.path.abspath("exts"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Machine Vision Toolbox'
-copyright = '2020-, Peter Corke'
-author = 'Peter Corke and Dorian Tsai'
+project = "Machine Vision Toolbox"
+copyright = "2020-, Peter Corke"
+author = "Peter Corke and Dorian Tsai"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = "1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,32 +35,34 @@ release = '1.0'
 # ones.
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax',
-    'matplotlib.sphinxext.plot_directive',
-    'sphinx.ext.coverage',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx_autorun',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx_autorun",
     "sphinx.ext.intersphinx",
     "sphinx_favicon",
-    'blockname',
+    "blockname",
 ]
 
-#autoclass_content = 'both' # use __init__ or class docstring
+# autoclass_content = 'both' # use __init__ or class docstring
 add_function_parentheses = False
 
 # options for spinx_autorun, used for inline examples
 #  choose UTF-8 encoding to allow for Unicode characters, eg. ansitable
 #  Python session setup, turn off color printing for SE3, set NumPy precision
 autorun_languages = {}
-autorun_languages['pycon_output_encoding'] = 'UTF-8'
-autorun_languages['pycon_input_encoding'] = 'UTF-8'
-autorun_languages['pycon_runfirst'] = """
+autorun_languages["pycon_output_encoding"] = "UTF-8"
+autorun_languages["pycon_input_encoding"] = "UTF-8"
+autorun_languages[
+    "pycon_runfirst"
+] = """
 from spatialmath import SE3
 SE3._color = False
 import numpy as np
@@ -69,16 +72,16 @@ ANSITable._color = False
 """
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 autosummary_generate = True
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 autosummary_imported_members = True
 add_module_name = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['test_*']
+exclude_patterns = ["test_*"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -86,25 +89,24 @@ exclude_patterns = ['test_*']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    'github_user': 'petercorke',
+    "github_user": "petercorke",
     #'github_repo': 'spatialmath-python',
     #'logo_name': False,
-    'logo_only': False,
+    "logo_only": False,
     #'description': 'Spatial maths and geometry for Python',
-    'display_version': True,
-    'prev_next_buttons_location': 'both',
-
-    }
-html_logo = '../../figs/VisionToolboxLogo_CircBlack.png'
+    "display_version": True,
+    "prev_next_buttons_location": "both",
+}
+html_logo = "../../figs/VisionToolboxLogo_CircBlack.png"
 html_show_sourcelink = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-html_last_updated_fmt = '%d-%b-%Y'
+html_last_updated_fmt = "%d-%b-%Y"
 
 # see https://stackoverflow.com/questions/9728292/creating-latex-math-macros-within-sphinx
 mathjax3_config = {
@@ -113,7 +115,7 @@ mathjax3_config = {
             # RVC Math notation
             #  - not possible to do the if/then/else approach
             #  - subset only
-            #"presup": [r"\,{}^{\scriptscriptstyle #1}\!", 1],
+            # "presup": [r"\,{}^{\scriptscriptstyle #1}\!", 1],
             "presup": [r"\,{}^{#1}\!", 1],
             # groups
             "SE": [r"\mathbf{SE}(#1)", 1],
@@ -141,16 +143,16 @@ mathjax3_config = {
             "q": r"\mathring{q}",
             "fq": [r"\presup{#1}\mathring{q}", 1],
         }
-   }
+    }
 }
 
 intersphinx_mapping = {
     "numpy": ("http://docs.scipy.org/doc/numpy/", None),
     "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
-    "matplotlib": ("http://matplotlib.sourceforge.net/", None),
+    "matplotlib": ("http://matplotlib.org/stable/", None),
     "open3d": ("http://www.open3d.org/docs/release/", None),
-    'opencv' : ('http://docs.opencv.org/2.4/', None),
-    "smtb": ("https://petercorke.github.io/spatialmath-python/", None),
+    "opencv": ("http://docs.opencv.org/2.4/", None),
+    "smtb": ("https://bdaiinstitute.github.io/spatialmath-python/", None),
     "pgraph": ("https://petercorke.github.io/pgraph-python/", None),
 }
 # maybe issues with cv2 https://stackoverflow.com/questions/30939867/how-to-properly-write-cross-references-to-external-documentation-with-intersphin
