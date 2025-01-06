2f9cd8b10e1c5f5fb1edca86b33f5f750a469177
Peter Corke <peter.i.corke@gmail.com>
2025-01-07 06:51:21 +1000

    add blob hierarchy

--- examples/blobs2.py
+++ examples/blobs2.py
@@ -15,5 +15,7 @@ b5 = blobs[5]
 print(b5)
 print(f"it has {len(b5.children)} child blobs")
 
+blobs.dotfile(show=True)
+
 labels, n = mb.labels_binary()
 labels.disp(title="label image", colorbar=True, block=True)
